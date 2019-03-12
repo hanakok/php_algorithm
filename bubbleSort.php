e6230de4fe31b98e6babb862f48e38654fc48304
hanakok <ld.pxo.id@gmail.com>
2019-03-12 23:37:37 +0900

    バブルソート修正&コメント追加

--- bubbleSort.php
+++ bubbleSort.php
@@ -11,8 +11,12 @@ Class BubbleSort {
     {
         $size = count($array);
 
-        for($i=0; $i<$size; $i++) {
-            for ($j=0; $j<$size; $j++) {
+        // 配列を先頭から最後まで見るループ
+        // 植木算なので$size-1回で良い
+        for($i=0; $i<$size-1; $i++) {
+            // 実際に比較を行う処理
+            // キーなし落ち防止のため、$j=1からはじまるので、ループ回数は実質植木算
+            for ($j=1; $j<$size; $j++) {
                 if ($array[$j-1] > $array[$j]) {
                     $tmp = $array[$j];
                     $array[$j] = $array[$j-1];
